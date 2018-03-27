bec7810db6c773e067dcc60c8b460f08780df92f
Fabio Araujo <faraujo@marketdata.com.br>
2018-03-27 07:25:34 -0300

    criado novo teste

--- src/test/scala/CubeCalculatorTest.scala
+++ src/test/scala/CubeCalculatorTest.scala
@@ -1,7 +1,11 @@
 import org.scalatest.FunSuite
 
 class CubeCalculatorTest extends FunSuite{
-  test("CubeCalculator.cube"){
+  test("CubeCalculator.cubeOf3"){
     assert(CubeCalculator.cube(3) === 27)
+  }
+
+  test("CubeCalculator.cubeOf0"){
+    assert(CubeCalculator.cube(0) === 0)
   }
 }
